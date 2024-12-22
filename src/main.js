5cb4d6153c9beb009d6cfdb4ee9b4ba055387500
jo ea <joea1755@yahoo.com>
2024-12-22 16:23:21 +0330

    a

--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ if (window.navigator){
         }
     }
 
+    const devicePixelRatio = window.devicePixelRatio;
+    const actualWidth = window.screen.width * devicePixelRatio;
+    const actualHeight = window.screen.height * devicePixelRatio;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
     document.getElementById('navigatorInfo').textContent = JSON.stringify(navigatorInfo, null, 2);
+    document.getElementById('a').textContent = `Window Size: ${windowWidth} x ${windowHeight}`
+    document.getElementById('b').textContent = `Device Resolution: ${actualWidth} x ${actualHeight}`
 
 } else {
     alert("not found")
